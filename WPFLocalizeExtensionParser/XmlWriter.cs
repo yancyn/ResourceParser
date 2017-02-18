11540d268c90a5303e262a4dcc28fa931db1870e
yancyn <yancyn@gmail.com>
2017-02-18 10:48:56 -0600

    Add placeholder for write header and footer in resource file

--- WPFLocalizeExtensionParser/XmlWriter.cs
+++ WPFLocalizeExtensionParser/XmlWriter.cs
@@ -14,15 +14,25 @@ namespace WPFLocalizeExtensionParser
         public XmlWriter()
         {
             this.builder = new StringBuilder();
+            AddHeader();
         }
-        public void Add(string root, string translation)
+        private void AddHeader()
+        {
+
+        }
+        public void AddNode(string root, string translation)
         {
             this.builder.AppendLine(string.Format(TAB + "<data name=\"{0}\" xml:space=\"preserve\">", root));
             this.builder.AppendLine(string.Format(TAB + TAB + "<value>{0}</value>", translation));
             this.builder.AppendLine(TAB + "</data>");
         }
+        private void AddFooter()
+        {
+            this.builder.AppendLine("<root>");
+        }
         public void Write(string outputFileName)
         {
+            // AddFooter();
             using (StreamWriter writer = new StreamWriter(outputFileName))
             {
                 writer.WriteLine(builder.ToString());
