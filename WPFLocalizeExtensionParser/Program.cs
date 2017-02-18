b08202efa9c699bdd4ab7e98c7cbb7e9a9e797ad
yancyn <yancyn@gmail.com>
2017-02-18 09:57:22 -0600

    Print out argument in console

--- WPFLocalizeExtensionParser/Program.cs
+++ WPFLocalizeExtensionParser/Program.cs
@@ -8,8 +8,17 @@ namespace WPFLocalizeExtensionParser
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <remarks></remarks>
         static void Main(string[] args)
         {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
             Console.WriteLine("Press any key to exit");
             Console.ReadKey(true);
         }
