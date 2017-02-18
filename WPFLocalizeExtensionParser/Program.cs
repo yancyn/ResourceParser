340ac2cb18b3ef6f0611c3fe4bde14756292d240
yancyn <yancyn@gmail.com>
2017-02-18 09:13:33 -0600

    Setup a console app

---
+++ WPFLocalizeExtensionParser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLocalizeExtensionParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
