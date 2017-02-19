41cedcac2ef3aa2082ed27e36655b53c81a79e50
yancyn <yancyn@gmail.com>
2017-02-19 12:42:47 -0600

    Passing parameter to parser from console app

--- WPFLocalizeExtensionParser/Program.cs
+++ WPFLocalizeExtensionParser/Program.cs
@@ -15,10 +15,29 @@ namespace WPFLocalizeExtensionParser
         /// <remarks></remarks>
         static void Main(string[] args)
         {
+            string inputFileName = string.Empty;
+            string outputFileName = string.Empty;
+
+            string parameter = string.Empty;
             foreach (string arg in args)
             {
+                switch (parameter)
+                {
+                    case "-s":
+                        inputFileName = arg;
+                        break;
+                    case "-o":
+                        outputFileName = arg;
+                        break;
+                }
+
                 Console.WriteLine(arg);
+                parameter = arg;
             }
+
+            Parser parser = new Parser(inputFileName, outputFileName);
+            parser.Process();
+
             Console.WriteLine("Press any key to exit");
             Console.ReadKey(true);
         }
